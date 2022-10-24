36def812aed5ab15202a06a9eb8896781b7d4a1f
LuraMoth <85266594+Luramoth@users.noreply.github.com>
2022-10-23 17:33:59 -0700

    make batch file to help run the script on startup

---
+++ run.bat
@@ -0,0 +1,2 @@
+::replave file paths with wherever everything is, and yes i did mispell python, yes i did notice it immidiatly, no i wont fix it
+C:/Users/fasre/AppData/Local/Programs/Python/Python310/python.exe "c:/Users/fasre/Programming/Pyhton/go to bed bitch/GoToBedBitch.py"
